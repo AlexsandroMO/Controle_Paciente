ceadcc67d59c14c5326ecc4dc4dd196f5e8d9d60
Alexsandro <sandrobass@hotmail.com>
2022-09-01 16:02:47 -0300

    stap 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask('app')
 
@@ -6,8 +6,21 @@ app = Flask('app')
 def home():
   return render_template('home.html')
 
-@app.route('/add_create')
+@app.route('/add_')
+def add_():
+  return render_template('cadastrar.html')
+
+@app.route('/add_create', methods=['POST', 'GET'])
 def add_create():
+  if request.method == 'POST':
+    result = request.form
+    name = result['name-paciente']
+    age = result['age-paciente']
+    gen = result['gen-paciente']
+    cpf = result['cpf-paciente']
+    p_pac = result['p-paciente']
+    a_pac = result['a-paciente']
+
   return render_template('cadastrar.html')
 
 #app.run(host='0.0.0.0', port=8080, debug=True)
